22f9f4cf6b0b27ef86490477cfc08c4ed7d470ba
tipstrying <296719064@qq.com>
2017-06-28 11:17:11 +0800

    2017/06/28

--- first_webpage/scripts/main.js
+++ first_webpage/scripts/main.js
@@ -1,3 +1,5 @@
+document.querySelector('h2').style.display = "none";
+ document.querySelector('textarea').style.display = "none";
 var i = 1;
 document.querySelector('img').onclick = function()
 {
@@ -6,15 +8,18 @@ document.querySelector('img').onclick = function()
     var type = ".jpg"
 
     document.querySelector('img').setAttribute('src', name + i.toString() + type);
-    if (i < 4 )
+    if (i < 5 )
     {
         i = i + 1;
     }
     else
     {
         alert('木有啦~~~~~~~')
+
         i = 4;
-         
+        document.querySelector('img').style.display = "none";
+        document.querySelector('h2').style.display = "inline";
+        document.querySelector('textarea').style.display = "inline";
     }
        
 }
